fb51e103e595a7850beef349963832756afcb437
@Sergi <sergitkeshelashvili@gmail.com>
2025-07-14 16:34:45 +0400

    Update init_database.sql

--- sql_netflix_data_analysis_project/init_database.sql
+++ sql_netflix_data_analysis_project/init_database.sql
@@ -1,26 +1,27 @@
--- Database and Table Setup
--- Creates the netflix_sql_project database and netflix table with appropriate columns and data types.
+-- 1. Creates the netflix_sql_project database
+-- This query creates a new database named 'netflix_sql_project' to store the Netflix data.
 
-CREATE DATABASE netflix_sql_project ;
+CREATE DATABASE netflix_sql_project;
 
-
--- Drops the netflix table if it already exists to avoid conflicts.
+-- 2. Drops the netflix table if it exists
+-- This ensures no conflicts occur by dropping the 'netflix' table if it already exists.
 
 DROP TABLE IF EXISTS netflix;
 
--- Creates the netflix table with columns;
+-- 3. Creates the netflix table with specified columns
+-- Defines the structure of the 'netflix' table with columns for show details like ID, type, title, etc.
 
 CREATE TABLE netflix (
-	show_id VARCHAR(5),
-	type VARCHAR(10),
-	title VARCHAR(250),
-	director VARCHAR(550),
-	casts VARCHAR(1050),
-	country VARCHAR(550),
-	date_added VARCHAR(55),
-	release_year INT,
-	rating VARCHAR(15),
-	duration VARCHAR(15),
-	listed_in VARCHAR(250),
-	description VARCHAR(550)
+    show_id VARCHAR(5),
+    type VARCHAR(10),
+    title VARCHAR(250),
+    director VARCHAR(550),
+    casts VARCHAR(1050),
+    country VARCHAR(550),
+    date_added VARCHAR(55),
+    release_year INT,
+    rating VARCHAR(15),
+    duration VARCHAR(15),
+    listed_in VARCHAR(250),
+    description VARCHAR(550)
 );
